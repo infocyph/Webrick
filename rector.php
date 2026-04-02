dcb4fb37a7eb7650c687dfd7e073983c55bcdcc4
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-04-02 15:50:27 +0600

    sync

--- rector.php
+++ rector.php
@@ -6,11 +6,14 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__.'/src',
-    ]);
+    $rectorConfig->paths([__DIR__ . '/src']);
+
+    $setConstant = SetList::class . '::PHP_' . PHP_MAJOR_VERSION . PHP_MINOR_VERSION;
+    if (!defined($setConstant)) {
+        $setConstant = SetList::class . '::PHP_84';
+    }
+
     $rectorConfig->sets([
-//        LevelSetList::UP_TO_PHP_84,
-        SetList::PHP_84,
+        constant($setConstant),
     ]);
 };
