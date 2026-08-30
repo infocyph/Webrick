caa9ecd38d56a246e4cc44d76c59e588eb63cf21
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-30 23:12:26 +0600

    feat: define runtime adapter contract

---
+++ src/Runtime/Http/RuntimeAdapterInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Webrick\Runtime\Http;
+
+use Infocyph\Webrick\Response\Response;
+
+interface RuntimeAdapterInterface
+{
+    public function capabilities(): RuntimeCapabilities;
+
+    public function context(mixed $nativeRequest = null, mixed $nativeResponse = null): RuntimeRequestContext;
+
+    public function write(Response $response, RuntimeRequestContext $context): void;
+}
