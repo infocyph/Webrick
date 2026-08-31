af1e9aadb71a42621be683cf1510e1c7540d8df5
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-01 00:37:07 +0600

    fix: bump fused matcher capture cache format

--- src/Router/Matching/FusedMatcher.php
+++ src/Router/Matching/FusedMatcher.php
@@ -15,7 +15,7 @@ final class FusedMatcher extends AbstractMatcher implements MatcherInterface
     use MatcherCacheLifecycleTrait;
     use MatcherFactoryTrait;
 
-    private const int INDEX_CACHE_VERSION = 8;
+    private const int INDEX_CACHE_VERSION = 9;
 
     /** @var array<string,array{0:string,1:?string}> */
     private array $alias = [];
