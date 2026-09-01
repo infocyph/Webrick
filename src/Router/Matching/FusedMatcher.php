333c160defae1dba5f716b03cab916165c387200
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-01 06:28:06 +0600

    perf: bump Fused cache for adaptive matcher IR

--- src/Router/Matching/FusedMatcher.php
+++ src/Router/Matching/FusedMatcher.php
@@ -15,7 +15,7 @@ final class FusedMatcher extends AbstractMatcher implements MatcherInterface
     use MatcherCacheLifecycleTrait;
     use MatcherFactoryTrait;
 
-    private const int INDEX_CACHE_VERSION = 11;
+    private const int INDEX_CACHE_VERSION = 12;
 
     /** @var array<string,array{0:string,1:?string}> */
     private array $alias = [];
